fb63ae672aecde345b64c835095f400aabb9b094
Rachid Jaada <rjaada42@gmail.com>
2026-04-10 21:52:56 +0200

    Add Bureaucrat class implementation with exception handling
    - Created Bureaucrat class with constructors, destructor, and assignment operator.
    - Implemented methods for getting name and grade, and for incrementing and decrementing grade.
    - Added exception classes for handling grade limits.
    - Implemented operator<< for Bureaucrat to output details.
    - Created Makefile for building the project.
    - Added main function to demonstrate Bureaucrat functionality and exception handling.

---
+++ CPP05/ex00/main.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaada <rjaada@student.42.fr>              #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-04-10 19:26:14 by rjaada            #+#    #+#             */
+/*   Updated: 2026-04-10 19:26:14 by rjaada           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main(void)
+{
+	try
+	{
+		Bureaucrat b1("Rachid", 42);
+		std::cout << b1;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		Bureaucrat b2("Jaada", 0);
+		std::cout << b2;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	try
+	{
+		Bureaucrat b3("Imane", 151);
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
+	
+
+
+
+
+	return (0);
+}
