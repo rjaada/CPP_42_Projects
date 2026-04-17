559673410aba0867f2d62f00d03144595c2d66a7
Rachid Jaada <rjaada42@gmail.com>
2026-04-17 08:59:00 +0200

    add AForm base class and ShrubberyCreationForm implementation

---
+++ CPP05/ex02/Bureaucrat.cpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaada <rjaada@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/10 18:11:59 by rjaada            #+#    #+#             */
+/*   Updated: 2026/04/13 21:26:18 by rjaada           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+Bureaucrat::Bureaucrat() : name("bureaucrat"), grade(75)
+{
+	std::cout << "Bureaucrat Default Constructor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int grade) : name(name),
+	grade(grade)
+{
+	std::cout << "Bureaucrat Constructor Called" << std::endl;
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : name(other.name),
+	grade(other.grade)
+{
+	std::cout << "Bureaucrat Copy Constructor Called" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &other)
+{
+	std::cout << "Bureaucrat Copy Assignment Called" << std::endl;
+	if (this != &other)
+		this->grade = other.grade;
+	return (*this);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (grade);
+}
+
+std::string Bureaucrat::getName() const
+{
+	return (name);
+}
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &b)
+{
+	out << b.getName() << ",bureaucrat grade " << b.getGrade() << "." << std::endl;
+	return (out);
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (grade <= 1)
+		throw GradeTooHighException();
+	grade--;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high");
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (grade >= 150)
+		throw GradeTooLowException();
+	grade++;
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low");
+}
+
+void Bureaucrat::signForm(Form &f)
+{
+	try 
+    {
+        f.beSigned(*this);
+        std::cout << getName() << " signed " << f.getName() << std::endl;
+    }
+    catch (std::exception &e) 
+    {
+        std::cout << getName() << " couldn’t sign " << f.getName() << " because " << e.what() << std::endl;
+    }
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat Destructor Called" << std::endl;
+}
---
+++ CPP05/ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjaada <rjaada@student.42.fr>              #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-04-17 04:19:48 by rjaada            #+#    #+#             */
+/*   Updated: 2026-04-17 04:19:48 by rjaada           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+# include "AForm.hpp"
+# include <iostream>
+# include <string>
+# include <exception>
+#include <fstream>
+
+class ShrubberyCreationForm : public AForm
+{
+  private:
+	std::string target;
+
+  public:
+	ShrubberyCreationForm(std::string const &target);
+	ShrubberyCreationForm(ShrubberyCreationForm const &other);
+	ShrubberyCreationForm &operator=(ShrubberyCreationForm const &other);
+	~ShrubberyCreationForm();
+	void executeAction() const;
+};
+
+#endif
