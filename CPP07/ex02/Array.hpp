4f5c930973f760368991ac2d4b4fea57237b1bcb
Rachid Jaada <rjaada42@gmail.com>
2026-05-11 11:28:53 +0200

    Refactor Array class: implement copy constructor, assignment operator, and exception handling; add Makefile for build automation

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -11,27 +11,64 @@
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
-# define ARRYA_HPP
+# define ARRAY_HPP
 
+# include <cstdlib>
+# include <ctime>
+# include <exception>
 # include <iostream>
 # include <string>
 
 template <typename T> class Array
 {
-private:
-	int size;
+  private:
+	unsigned int s;
 	T *myarray;
-public:
-	Array();
-	Array(unsigned int n);
-	Array(Array const &other);
-	Array &operator=(const Array &copy);
-	~Array();
-	int size();
+
+  public:
+	Array() : s(0), myarray(NULL)
+	{
+	}
+	Array(unsigned int n) : s(n), myarray(new T[n])
+	{
+	}
+	Array(Array const &other) : s(other.s), myarray(new T[s])
+	{
+		for (unsigned int i = 0; i < s; i++)
+			myarray[i] = other.myarray[i];
+	}
+	Array &operator=(const Array &copy)
+	{
+		if (this != &copy)
+		{
+			delete[] myarray;
+			s = copy.s;
+			myarray = new T[s];
+			for (unsigned int i = 0; i < s; i++)
+				myarray[i] = copy.myarray[i];
+		}
+		return (*this);
+	}
+	~Array()
+	{
+		delete[] myarray;
+	}
+	unsigned int size() const
+	{
+		return (s);
+	}
+	T &operator[](unsigned int index)
+	{
+		if (index >= s)
+			throw std::exception();
+		return (myarray[index]);
+	}
+	const T &operator[](unsigned int index) const
+	{
+		if (index >= s)
+			throw std::exception();
+		return (myarray[index]);
+	}
 };
-
-
-
-
 
 #endif
